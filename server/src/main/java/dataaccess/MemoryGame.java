e6a1e07dac5dad2caf3e5e46faae449908ebce23
Armando <armandocarreon@outlook.com>
2025-02-24 17:00:26 -0700

    Phase 3: MemoryGame update

--- server/src/main/java/dataaccess/MemoryGame.java
+++ server/src/main/java/dataaccess/MemoryGame.java
@@ -1,4 +1,42 @@
 package dataaccess;
 
-public class MemoryGame {
+import model.GameData;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class MemoryGame implements GameDAO{
+
+    public List<GameData> gameList = new ArrayList<>();
+
+    @Override
+    public void createGame(GameData game) {
+        gameList.add(game);
+    }
+
+    @Override
+    public GameData getGame(int gameID) {
+        return gameList.get(gameID);
+    }
+
+    @Override
+    public Collection<GameData> listGames() {
+        return gameList;
+    }
+
+    @Override
+    public void updateGame(int gameID, GameData game) {
+
+    }
+
+    @Override
+    public void clearGame(int gameID) {
+        gameList.clear();
+    }
+
+    @Override
+    public void clearAll() {
+
+    }
 }

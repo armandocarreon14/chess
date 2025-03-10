6c8497207ca53cae72f9771925f917dd2ca1a87c
Armando <armandocarreon@outlook.com>
2025-03-10 16:27:38 -0600

    Phase 4 SQLAuth

--- server/src/main/java/dataaccess/SQLAuth.java
+++ server/src/main/java/dataaccess/SQLAuth.java
@@ -1,31 +1,108 @@
 package dataaccess;
 
 import model.AuthData;
+import passoff.exception.ResponseParseException;
 
-import java.util.UUID;
+import java.sql.SQLException;
+
+import static java.sql.Statement.RETURN_GENERATED_KEYS;
+
 
 public class SQLAuth implements AuthDAO {
-    private static String generateToken() {
-        return UUID.randomUUID().toString();
+
+    public SQLAuth() throws DataAccessException {
+        configureDatabase();
+    }
+
+    private void configureDatabase() throws DataAccessException {
+        DatabaseManager.createDatabase();
+        try (var conn = DatabaseManager.getConnection()) {
+            for (var statement : createStatements) {
+                try (var preparedStatement = conn.prepareStatement(statement)) {
+                    preparedStatement.executeUpdate();
+                }
+            }
+        } catch (SQLException ex) {
+            throw new DataAccessException(500, "Unable to configure database: %s" +ex.getMessage());
+        }
+    }
+
+    private final String[] createStatements = {
+                    """
+        CREATE TABLE IF NOT EXISTS `auths` (
+        `authToken` varchar(256) NOT NULL,
+        `username` varchar(256) NOT NULL,
+        PRIMARY KEY (`authToken`),
+        INDEX(username)
+        )
+        """
+        };
+
+    private int executeUpdate(String statement, Object... params) throws DataAccessException {
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var ps = conn.prepareStatement(statement, RETURN_GENERATED_KEYS)) {
+                for (var i = 0; i < params.length; i++) {
+                    var param = params[i];
+                    if (param instanceof String p) ps.setString(i + 1, p);
+                }
+                ps.executeUpdate();
+
+                var rs = ps.getGeneratedKeys();
+                if (rs.next()) {
+                    return rs.getInt(1);
+                }
+
+                return 0;
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException(500, String.format("unable to update database: %s, %s", statement, e.getMessage()));
+        }
     }
 
     @Override
-    public void createAuth(AuthData auth) throws DataAccessException {
-
+    public void createAuth(AuthData auth) {
+        var statement = "INSERT INTO auths (authToken, username) VALUES (?, ?)";
+        try {
+            executeUpdate(statement, auth.authToken(), auth.username());
+        }
+        catch (Throwable ex) {
+            System.out.println(ex.getMessage());
+        }
     }
 
     @Override
     public AuthData getAuth(String authToken) throws DataAccessException {
+        try (var conn = DatabaseManager.getConnection()){
+            var statement = "SELECT * FROM auths WHERE authToken=?";
+            try (var ps = conn.prepareStatement(statement)) {
+                ps.setString(1, authToken);
+                var rs = ps.executeQuery();
+                if (rs.next()) {
+                    return new AuthData(rs.getString("authToken"), rs.getString("username"));
+                }
+            }
+        }
+        catch (Exception e) {
+            throw new DataAccessException(500, String.format("Unable to read data: %s", e.getMessage()));
+        }
         return null;
     }
 
     @Override
     public void clearAuth(String authToken) throws DataAccessException {
+        var statement = "DELETE FROM auths WHERE authToken=?";
+        try {
+            executeUpdate(statement, authToken);
+        }
+        catch (Exception e) {
+            throw new DataAccessException(500, String.format("Unable to read data: %s", e.getMessage()));
+        }
 
     }
 
     @Override
     public void clearAllAuth() throws DataAccessException {
-
+        var statement = "TRUNCATE auth";
+        executeUpdate(statement);
     }
 }

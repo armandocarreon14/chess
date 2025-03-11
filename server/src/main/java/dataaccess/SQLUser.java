ba32e44de354a94f1a8b8c7ca80ab6c85fbaee2e
Armando <armandocarreon@outlook.com>
2025-03-10 18:37:25 -0600

    Phase 4 SQLUser start

--- server/src/main/java/dataaccess/SQLUser.java
+++ server/src/main/java/dataaccess/SQLUser.java
@@ -1,4 +1,22 @@
 package dataaccess;
 
-public class SQLUser {
+import model.UserData;
+
+public class SQLUser  implements UserDAO{
+
+
+    @Override
+    public void createUser(UserData userData) throws DataAccessException {
+
+    }
+
+    @Override
+    public UserData getUser(String username) throws DataAccessException {
+        return null;
+    }
+
+    @Override
+    public void clear() throws DataAccessException {
+
+    }
 }

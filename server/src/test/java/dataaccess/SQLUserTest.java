4537870dbcc56f485f06bc84d1bb123c024741e7
Armando <armandocarreon@outlook.com>
2025-03-12 17:25:42 -0600

    Phase 4 SQLUser Test finishing test I made

--- server/src/test/java/dataaccess/SQLUserTest.java
+++ server/src/test/java/dataaccess/SQLUserTest.java
@@ -1,8 +1,12 @@
 package dataaccess;
 
+import chess.ChessGame;
+import model.GameData;
 import model.UserData;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 
 public class SQLUserTest {
@@ -19,9 +23,51 @@ public class SQLUserTest {
     public void createUserValid() throws DataAccessException {
         SQLUser sqlUser = new SQLUser();
         UserData userData = new UserData("username", "passweord", "email");
+        sqlUser.createUser(userData);
+
+        UserData user = sqlUser.getUser("username");
+        assertEquals("username", user.username());
     }
 
+    @Test
+    public void createUserInvalid() throws DataAccessException {
+        SQLUser sqlUser = new SQLUser();
+        UserData userData = new UserData("username", "passweord", "email");
 
+        sqlUser.createUser(userData);
+        assertThrows(DataAccessException.class, () -> {
+            sqlUser.createUser(userData);
+        });
+    }
+
+    @Test
+    public void getUserValid() throws DataAccessException {
+        SQLUser sqlUser = new SQLUser();
+        UserData userData = new UserData("username", "passweord", "email");
+        sqlUser.createUser(userData);
+
+        UserData user = sqlUser.getUser("username");
+        assertEquals("username", user.username());
+    }
+
+    @Test
+    public void getUserInvalid() throws DataAccessException {
+        SQLUser sqlUser = new SQLUser();
+
+        UserData user = sqlUser.getUser("username");
+        assertNull(user);
+    }
+
+    @Test
+    public void clearValid() throws DataAccessException {
+        SQLUser sqlUser = new SQLUser();
+        UserData userData = new UserData("username", "passweord", "email");
+        sqlUser.createUser(userData);
+
+        sqlUser.clear();
+        UserData user = sqlUser.getUser("username");
+        assertNull(user);
+    }
 
 
 }

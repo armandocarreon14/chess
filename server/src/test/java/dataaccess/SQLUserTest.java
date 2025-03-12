921715e28090f6b7aa6f448c43a606c35cf83eba
Armando <armandocarreon@outlook.com>
2025-03-12 16:31:29 -0600

    Phase 4 SQLGame Test

--- server/src/test/java/dataaccess/SQLUserTest.java
+++ server/src/test/java/dataaccess/SQLUserTest.java
@@ -1,6 +1,27 @@
 package dataaccess;
 
+import chess.ChessGame;
+import model.GameData;
+import model.UserData;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
 public class SQLUserTest {
+
+    private SQLUser userDAO;
+
+    @BeforeEach
+    void setUp() throws DataAccessException {
+        userDAO = new SQLUser();
+        userDAO.clear();
+    }
+
+    @Test
+    public void createUserValid() throws DataAccessException {
+        SQLUser sqlUser = new SQLUser();
+        UserData userData = new UserData("username", "passweord", "email");
+    }
 
 }
 

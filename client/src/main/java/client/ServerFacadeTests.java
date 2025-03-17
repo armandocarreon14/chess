6aadbdf988d6194a40c5e33c01527600692bc2d3
Armando <armandocarreon@outlook.com>
2025-03-17 13:29:34 -0600

    Phase 5: starter code

---
+++ client/src/main/java/client/ServerFacadeTests.java
@@ -0,0 +1,31 @@
+package client;
+
+import org.junit.jupiter.api.*;
+import server.Server;
+
+
+public class ServerFacadeTests {
+
+    private static Server server;
+
+    @BeforeAll
+    public static void init() {
+        server = new Server();
+        var port = server.run(0);
+        System.out.println("Started test HTTP server on " + port);
+    }
+
+    @AfterAll
+    static void stopServer() {
+        server.stop();
+    }
+
+
+    /// Replace the sampleTest method with your own unit tests.
+    @Test
+    public void sampleTest() {
+        Assertions.assertTrue(true);
+    }
+
+
+}
